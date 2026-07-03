0da4cd67d0905997d8ac86331b9d1a9bc45f032b
haripriya-kuruba <haripriya7897k@gmail.com>
2026-07-03 13:20:05 +0530

    Create insert_data.sql

---
+++ Project-1-Employee-Management/insert_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO Departments VALUES
+(1,'Engineering'),
+(2,'Finance'),
+(3,'HR');
+
+INSERT INTO Employees VALUES
+(101,'John',1,'2022-01-15'),
+(102,'Alice',2,'2021-06-10'),
+(103,'David',1,'2023-03-20'),
+(104,'Sophia',3,'2020-09-01');
+
+INSERT INTO Salaries VALUES
+(101,85000,'2025-01-01'),
+(102,75000,'2025-01-01'),
+(103,90000,'2025-01-01'),
+(104,70000,'2025-01-01');
